fb93176eeac9c76220ca9cb109b5ebe328b6acfe
L. Kidd <130194770+SquirrelsAtARave@users.noreply.github.com>
2023-08-14 20:00:02 -0700

    homeroutes for homepage, login, profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,36 @@
 const router = require('express').Router();
+
+//homepage route
+route.get("/", async(req, res)=>{
+    try{
+        res.render("homepage")
+
+    }catch(err) {
+        console.error(err)
+    }
+})
+
+//login route
+route.get("/login", async(req, res)=>{
+    try{
+        res.render("login")
+
+    }catch(err) {
+        console.error(err)
+    }
+})
+
+//profile route
+route.get("/profile", async(req, res)=>{
+    try{
+        res.render("profile")
+
+    }catch(err) {
+        console.error(err)
+    }
+})
+
+
 module.exports = router;
+
+

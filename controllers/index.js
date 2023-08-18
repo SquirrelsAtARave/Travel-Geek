77bcc30c9838108d09c6f7bf7b038a05576c4ff2
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-17 19:09:04 -0700

    Created Intinirary and Budget button in profile.handlebars file

--- controllers/index.js
+++ controllers/index.js
@@ -7,31 +7,3 @@ router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
-/* 
-document.addEventListener("DOMContentLoaded", () => {
-    const appElement = document.getElementById("app");
-    const loginFormTemplate = Handlebars.compile(document.getElementById("loginFormTemplate").innerHTML);
-    const signUpFormTemplate = Handlebars.compile(document.getElementById("signUpFormTemplate").innerHTML);
-
-    appElement.innerHTML = loginFormTemplate();
-    appElement.innerHTML += signUpFormTemplate();
-
-    const loginForm = document.getElementById("loginForm");
-    const signUpForm = document.getElementById("signUpForm");
-
-    loginForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const loginUsername = document.getElementById("loginUsername").value;
-        const loginPassword = document.getElementById("loginPassword").value;
-
-        // Perform login logic (e.g., send data to server)
-    });
-
-    signUpForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const signupUsername = document.getElementById("signupUsername").value;
-        const signupPassword = document.getElementById("signupPassword").value;
-
-        // Perform sign-up logic (e.g., send data to server)
-    });
-});*/

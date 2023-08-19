3303fa5ebc9f433d0bb263bafc1f4bc942d3cebc
L. Kidd <130194770+SquirrelsAtARave@users.noreply.github.com>
2023-08-18 21:21:33 -0700

    troubleshooting login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,6 +45,7 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
+    console.log(err)
     res.status(400).json(err);
   }
 });
@@ -80,6 +81,7 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
